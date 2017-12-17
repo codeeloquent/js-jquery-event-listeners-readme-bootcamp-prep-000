8849107a0f0beb000279d4872ab7468eb2e05d65
Ryan Eckerle <e_ryan1@yahoo.com>
2017-12-17 14:09:12 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,27 @@
 //define functions here
 
+function getIt(){
+  $('p').on("click", function(){
+    alert("Hey!");
+  });
+}
+
+function frameIt(){
+  $('img').on("load", function() {
+    $('img').addClass("tasty");
+  });
+}
+
+function pressIt(){
+  $('#typing').on("kdydown", function(press){
+    if(press.which === 71){
+      alert("You pressed dwon the G key.");
+    }
+  });
+}
+
 $(document).ready(function(){
 
 // call functions here
-
+  
 });
